d5ed1ed13d1e42f4070f433e65a26b7324deb309
CarlitoMG <carlito.67a@gmail.com>
2025-08-13 19:31:53 -0300

    Arquivo Iniciais

---
+++ src/test/java/com/programacao/web/fatec/Api_Fatec/ApiFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacao.web.fatec.Api_Fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
